1f6ed3e5ba25eac4d01796ed14ed72b393175b33
Baturin Kirill <batyrin2001yandex.ru>
2023-03-09 03:06:33 +0300

    Basic commands

---
+++ commands.py
@@ -0,0 +1,8 @@
+START = '/start'
+HELP = '/help'
+
+
+def checkCommand(command, checked):
+    if command == checked:
+        return True
+    return False
--- main.py
+++ main.py
@@ -2,13 +2,37 @@ import telebot
 import os
 from dotenv import load_dotenv, find_dotenv
 
+import commands
+from commands import checkCommand
+
 load_dotenv(find_dotenv())
+
 
 bot = telebot.TeleBot(os.getenv('TOKEN'))
 
 
+@bot.message_handler(commands=['start', 'help'])
+def command_handler(message):
+    bot_msg = ''
+    if checkCommand(message.text, commands.START):
+        bot_msg = f'Привет, <b>{message.from_user.first_name}</b>!\n' \
+                  f'Введи команду /help для просмотра справки.'
+    elif checkCommand(message.text, commands.HELP):
+        bot_msg = 'Данный раздел находится в стадии разработки.'
+
+    bot.send_message(message.chat.id, bot_msg, parse_mode='html')
+
+
+@bot.message_handler()
+def other_handler(message):
+    bot_msg = 'Пока что я не научился отвечать на такие запросы.\n' \
+              'Введите /help для просмотра списка доступных комманд.'
+
+    bot.send_message(message.chat.id, bot_msg, parse_mode='html')
+
+
 def main():
-    print(bot.token)
+    bot.polling(none_stop=True)
 
 
 if __name__ == '__main__':
